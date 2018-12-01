e6aa5405e56f6f471f3405b814c43377197ecbcc
Teofilo Monteiro <teofilo.monteiro@mindera.com>
2018-12-01 15:51:50 +0000

    Deprecate scan side reading from input

--- src/main/java/com/teofilom/triangle/TriangleApp.java
+++ src/main/java/com/teofilom/triangle/TriangleApp.java
@@ -3,25 +3,24 @@ package com.teofilom.triangle;
 import com.teofilom.triangle.models.Triangle;
 import com.teofilom.triangle.models.TriangleBuilder;
 
-import java.util.Scanner;
-
 public class TriangleApp {
     public static void main(String[] args) {
         Double sideA, sideB, sideC;
 
-        if (args.length > 0 && args.length < 4) {
-            sideA = Double.valueOf(args[0]);
-            sideB = Double.valueOf(args[1]);
-            sideC = Double.valueOf(args[2]);
-        } else {
-            Scanner scan = new Scanner(System.in);
-
-            System.out.println("Enter the side A: ");
-            sideA = scan.nextDouble();
-            System.out.println("Enter the side B: ");
-            sideB = scan.nextDouble();
-            System.out.println("Enter the side C: ");
-            sideC = scan.nextDouble();
+        switch (args.length) {
+            case 3:
+                try {
+                    sideA = Double.valueOf(args[0]);
+                    sideB = Double.valueOf(args[1]);
+                    sideC = Double.valueOf(args[2]);
+                } catch (NumberFormatException e) {
+                    printHelp();
+                    return;
+                }
+                break;
+            default:
+                printHelp();
+                return;
         }
 
         Triangle triangle = new TriangleBuilder()
@@ -31,5 +30,17 @@ public class TriangleApp {
             .build();
 
         System.out.println(triangle.type());
+    }
+
+    public static void printHelp() {
+        System.out.println("Usage:");
+        System.out.println("\n\tjava -jar triangle [-h]");
+        System.out.println("\nor");
+        System.out.println("\n\tjava -jar triangle [sideA] [sideB] [sideC]");
+        System.out.println("\nArguments:");
+        System.out.println("\t-h, --help\t\t\tShow this help message and exit.");
+        System.out.println("\tsideA\t\t\t\ttriangle side A length(float number)");
+        System.out.println("\tsideB\t\t\t\ttriangle side B length(float number)");
+        System.out.println("\tsideC\t\t\t\ttriangle side C length(float number)");
     }
 }
